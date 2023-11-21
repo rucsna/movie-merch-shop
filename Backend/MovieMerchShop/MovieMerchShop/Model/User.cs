bd798b18caacbcda9627a98b711b380e55b2e90c
Anna Pomichal <rucsna@gmail.com>
2023-11-21 13:40:08 +0100

    feat: add User class

---
+++ Backend/MovieMerchShop/MovieMerchShop/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieMerchShop.Model;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public DateTime BirthDate { get; set; }
+    //public ICollection<Order> Orders { get; set; }
+    //public ICollection<MerchItem> WishList { get; set; }
+    public decimal Balance { get; set; }
+}
