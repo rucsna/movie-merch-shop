dd5d060cd07e3973041a05ad02d223fa5ff40681
Anna Pomichal <rucsna@gmail.com>
2023-11-22 11:39:56 +0100

    refactor: set public and private properties for User class

--- Backend/MovieMerchShop/MovieMerchShop/Model/User.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Model/User.cs
@@ -4,9 +4,22 @@ public class User
 {
     public Guid Id { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
     public DateTime BirthDate { get; set; }
-    //public ICollection<Order> Orders { get; set; }
-    //public ICollection<MerchItem> WishList { get; set; }
-    public decimal Balance { get; set; }
+    private string _password;
+    private decimal _balance;
+    private readonly ICollection<Order> _orders;
+    private readonly ICollection<MerchItem> _wishList;
+
+    public User(string userName, DateTime birthDate, string password)
+    {
+        Id = Guid.NewGuid();
+        UserName = userName;
+        BirthDate = birthDate;
+        _password = password;
+        _balance = 0;
+        _orders = new List<Order>();
+        _wishList = new List<MerchItem>();
+    }
+
+    
 }
