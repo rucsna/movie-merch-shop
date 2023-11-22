f609b19d93030712ef446efb29b6f10488332916
kovacskitti <kovacskitti001@gmail.com>
2023-11-22 10:53:22 +0100

    refactor: change type of _actors to ICollection<string>

--- Backend/MovieMerchShop/MovieMerchShop/Model/Movie.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Model/Movie.cs
@@ -10,26 +10,27 @@ namespace MovieMerchShop.Model
         public int Year { get; init; }
         public string PosterLink { get; set; }
         public string Plot { get; set; }
-        private ICollection<Actor> _actors;
+        private ICollection<string> _actors;
         private ICollection<string> _genres;
         private ICollection<MerchItem> _merches;
+        
 
         public Movie(int year, string title)
         {
             Title = title;
             Year = year;
             Id = Guid.NewGuid();
-            _actors = new List<Actor>();
+            _actors = new List<string>();
             _merches = new List<MerchItem>();
             _genres = new List<string>();
         }
         
-        public void AddActor(Actor actor)
+        public void AddActor(string actor)
         {
             _actors.Add(actor);
         }
 
-        public IEnumerable<Actor> GetActors()
+        public IEnumerable<string> GetActors()
         {
             return _actors;
         }
