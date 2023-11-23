51a82477a1cf9398e1c298b243e3a280c9dcd0f4
kovacskitti <kovacskitti001@gmail.com>
2023-11-23 12:40:41 +0100

    feat: create browser router

--- Frontend/vite-project/src/main.jsx
+++ Frontend/vite-project/src/main.jsx
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/products",
+        element: <Landing />,
+      },
+      {
+        path: "/:movieId/products",
+        element: <ProductsByMovie />,
+      },
+      {
+        path: "/:movieId/products/:filter",
+        element: <ProductsByCategories />,
+      },
+      {
+        path: "/:itemId",
+        element: <Item />,
+      },
+      {
+        path: "/:userId/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/registration",
+        element: <Registration />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
