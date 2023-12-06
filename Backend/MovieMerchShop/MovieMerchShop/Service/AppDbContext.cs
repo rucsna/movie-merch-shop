b44dc69493d2e763e0a8d759ae01b9cc88ba025a
kovacskitti <kovacskitti001@gmail.com>
2023-12-06 14:53:23 +0100

    feat: add OnModelCreating to AppDbContext

--- Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
@@ -13,4 +13,9 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mug>().HasBaseType<MerchItem>();
+    }
 }
