d0fc5631e09824702eb2b70464d03f3ab5b03623
Anna Pomichal <rucsna@gmail.com>
2023-12-07 11:20:25 +0100

    refactor: add values to MerchItems table

--- Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
@@ -8,14 +8,21 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Movie> Movies { get; set; }
-    public DbSet<MerchItem> Items { get; set; }
-    
+    public DbSet<MerchItem> MerchItems { get; set; }
+    // public DbSet<Mug> Mugs { get; set; }
+    // public DbSet<Shirt> Shirts { get; set; }
+    // public DbSet<Poster> Posters { get; set; }
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Mug>().HasBaseType<MerchItem>();
+        modelBuilder.Entity<MerchItem>()
+            .HasDiscriminator<string>("ItemType")
+            .HasValue<Mug>("Mug")
+            .HasValue<Poster>("Poster")
+            .HasValue<Shirt>("Shirt");
     }
 }
