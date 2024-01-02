1e7ae1547826f5878be56a2b261d26fb3c4690be
Anna Pomichal <rucsna@gmail.com>
2024-01-02 11:54:39 +0100

    refactor: add ItemType property to MerchItem

--- Backend/MovieMerchShop/MovieMerchShop/Model/MerchItem.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Model/MerchItem.cs
@@ -3,6 +3,7 @@
 public class MerchItem
 {
     public Guid Id { get; set; }
+    public ItemType Type { get; set; }
     public decimal Price { get; set; }
     public Guid MovieId { get; set; }
     public uint Quantity { get; set; }
