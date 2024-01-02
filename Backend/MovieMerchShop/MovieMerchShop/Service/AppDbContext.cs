0193bf98d55f91bdc65ce0349b983ecd2b1c09dd
Anna Pomichal <rucsna@gmail.com>
2024-01-02 11:33:28 +0100

    refactor: simplify DbContext

--- Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Service/AppDbContext.cs
@@ -9,20 +9,11 @@ public class AppDbContext : DbContext
     public DbSet<Order> Orders { get; set; }
     public DbSet<Movie> Movies { get; set; }
     public DbSet<MerchItem> MerchItems { get; set; }
-    // public DbSet<Mug> Mugs { get; set; }
-    // public DbSet<Shirt> Shirts { get; set; }
-    // public DbSet<Poster> Posters { get; set; }
+    public DbSet<Mug> Mugs { get; set; }
+    public DbSet<Shirt> Shirts { get; set; }
+    public DbSet<Poster> Posters { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<MerchItem>()
-            .HasDiscriminator<string>("ItemType")
-            .HasValue<Mug>("Mug")
-            .HasValue<Poster>("Poster")
-            .HasValue<Shirt>("Shirt");
     }
 }
