5f8c3d3414cd635cb5ef89cdfe86e68231b4b814
Anna Pomichal <rucsna@gmail.com>
2024-01-03 14:24:43 +0100

    refactor: add role to IAuthService.cs

--- Backend/MovieMerchShop/MovieMerchShop/Service/Authentication/IAuthService.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Service/Authentication/IAuthService.cs
@@ -2,7 +2,7 @@ namespace MovieMerchShop.Service.Authentication;
 
 public interface IAuthService
 {
-    Task<AuthResult> RegisterAsync(string email, string userName, string password, DateTime birthDate, string address);
+    Task<AuthResult> RegisterAsync(string email, string userName, string password, DateTime birthDate, string address, string role);
     Task<AuthResult> LoginAsync(string userName, string password);
     Task<AuthResult> DeactivateAsync(string email, string password);
 }
