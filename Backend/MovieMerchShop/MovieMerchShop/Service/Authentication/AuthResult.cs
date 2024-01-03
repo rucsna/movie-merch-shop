39807c688ea23920b8cadf56e3804638528c21a1
Anna Pomichal <rucsna@gmail.com>
2024-01-03 09:22:59 +0100

    feat: create AuthResult.cs

---
+++ Backend/MovieMerchShop/MovieMerchShop/Service/Authentication/AuthResult.cs
@@ -0,0 +1,11 @@
+namespace MovieMerchShop.Service.Authentication;
+
+public record AuthResult(
+    bool Success,
+    string Email,
+    string UserName,
+    string Token
+)
+{
+    public readonly Dictionary<string, string> ErrorMessages = new();
+}
