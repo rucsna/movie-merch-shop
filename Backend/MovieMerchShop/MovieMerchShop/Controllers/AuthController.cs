907fe8ff11265608537c448502d928af5eb996d5
Anna Pomichal <rucsna@gmail.com>
2024-01-03 14:26:02 +0100

    refactor: add role to Register method

--- Backend/MovieMerchShop/MovieMerchShop/Controllers/AuthController.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MovieMerchShop.Contracts;
 using MovieMerchShop.Service.Authentication;
@@ -9,12 +10,15 @@ namespace MovieMerchShop.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(IAuthService authService)
+    public AuthController(IAuthService authService, IConfiguration configuration)
     {
         _authService = authService;
+        _configuration = configuration;
     }
 
+    [Authorize(Roles="User, Admin")]
     [HttpPost("Register")]
     public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
     {
@@ -23,8 +27,9 @@ public class AuthController : ControllerBase
             return BadRequest(ModelState);
         }
 
+        var userRole = _configuration["RoleSettings:UserRole"];
         var result = await _authService.RegisterAsync(request.Email, request.UserName, request.Password,
-            request.BirthDate, request.Address);
+            request.BirthDate, request.Address, userRole!);
         if (!result.Success)
         {
             AddErrors(result);
@@ -34,6 +39,7 @@ public class AuthController : ControllerBase
         return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
     }
 
+    [Authorize(Roles="User, Admin")]
     [HttpPost("Login")]
     public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
     {
@@ -52,6 +58,7 @@ public class AuthController : ControllerBase
         return Ok(new AuthResponse(result.Email, result.UserName, result.Token));
     }
 
+    [Authorize(Roles="User, Admin")]
     [HttpPatch("DeactivateAccount")]
     public async Task<ActionResult<DeactivationResponse>> Deactivate([FromBody] AuthRequest request)
     {
