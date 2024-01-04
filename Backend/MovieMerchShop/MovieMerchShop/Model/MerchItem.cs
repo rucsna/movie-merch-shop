cc838fe979ff4e524a374dd11163b9ddba228d88
kovacskitti <kovacskitti001@gmail.com>
2024-01-04 10:19:24 +0100

    feat: change the type of Quantity

--- Backend/MovieMerchShop/MovieMerchShop/Model/MerchItem.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Model/MerchItem.cs
@@ -6,9 +6,10 @@ public class MerchItem
     public ItemType Type { get; set; }
     public decimal Price { get; set; }
     public Guid MovieId { get; set; }
-    public uint Quantity { get; set; }
+    public int Quantity { get; set; }
 
     public MerchItem()
     {
     }
+    
 }
