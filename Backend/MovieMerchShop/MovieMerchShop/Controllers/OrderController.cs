d6a21524ad26bd8fbbbfed18794a00728724235b
Anna Pomichal <rucsna@gmail.com>
2024-01-16 13:59:32 +0100

    feat: implement IOrderRepository interface

--- Backend/MovieMerchShop/MovieMerchShop/Controllers/OrderController.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Controllers/OrderController.cs
@@ -2,28 +2,34 @@
 using MovieMerchShop.Model;
 using MovieMerchShop.Service;
 using System.Linq;
+using MovieMerchShop.Contracts;
 using MovieMerchShop.Data;
+using MovieMerchShop.Service.Repository;
 
 namespace MovieMerchShop.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
-
-public class OrderController: ControllerBase
+public class OrderController : ControllerBase
 {
     private readonly AppDbContext _context;
-    
-    public OrderController(AppDbContext context)
+    private readonly IMerchItemRepository _itemRepository;
+    private readonly IOrderRepository _orderRepository;
+
+    public OrderController(AppDbContext context, IMerchItemRepository itemRepository, IOrderRepository orderRepository)
     {
         _context = context;
+        _itemRepository = itemRepository;
+        _orderRepository = orderRepository;
     }
-    
+
     [HttpGet("GetAllOrders")]
     public ActionResult<IEnumerable<Order>> GetAllOrders()
     {
         List<Order> orders = _context.Orders.ToList();
         return Ok(orders);
     }
-    
+
     [HttpGet("GetOrderById/{id}")]
     public ActionResult<Order> GetOrderById(Guid id)
     {
@@ -36,7 +42,7 @@ public class OrderController: ControllerBase
 
         return Ok(order);
     }
-    
+
     [HttpGet("GetOrderByUserId/{userid}")]
     public ActionResult<Order> GetOrderByUserId(Guid userid)
     {
@@ -44,31 +50,60 @@ public class OrderController: ControllerBase
 
         if (orders == null)
         {
-            return NotFound(); 
+            return NotFound();
         }
 
         return Ok(orders);
     }
 
     [HttpPost("AddOrder")]
-    public ActionResult<Order> AddOrder(Order newOrder)
+    public async Task<ActionResult<Order>> AddOrderAsync([FromBody] OrderRequest request)
     {
-        if (newOrder == null)
+        if (request.OrderedItemIds.Count == 0)
         {
-            return BadRequest(); 
+            return NotFound("The cart is empty");
         }
 
-        _context.Orders.Add(newOrder);
-        _context.SaveChanges();
-        return Content(newOrder.Id.ToString());
-        // return CreatedAtAction(nameof(GetOrderById), new { id = newOrder.Id }, newOrder);
+        var orderedItems = new List<MerchItem>();
+        
+        foreach (var itemId in request.OrderedItemIds)
+        {
+            var itemToAddOrder = await _itemRepository.GetItemByIdAsync(itemId);
+            if (itemToAddOrder == null)
+            {
+                return NotFound("There is no such item in the database");
+            }
+            orderedItems.Add(itemToAddOrder);
+            
+            if (itemToAddOrder.Quantity < 1)
+            {
+                return BadRequest($"Not enough item in the store");
+            }
+
+            itemToAddOrder.Quantity -= 1;
+            
+            await _itemRepository.UpdateItemAsync(itemToAddOrder);
+            //return Ok($"Item with id {itemToAddOrder.Id} updated quantity is {itemToAddOrder.Quantity}");
+        }
+        
+        var newOrder = new Order
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = request.UserId,
+                    Items = orderedItems,
+                    OrderSum = request.OrderSum,
+                    OrderTime = DateTime.Now
+                };
+
+        await _orderRepository.CreateNewOrderAsync(newOrder);
+        return Ok(newOrder);
     }
-    
+
     [HttpDelete("DeleteOrderById/{orderid}")]
     public ActionResult DeleteOrderById(Guid orderid)
     {
         var orderToDelete = _context.Orders.FirstOrDefault(order => order.Id == orderid);
-    
+
         if (orderToDelete == null)
         {
             return NotFound();
