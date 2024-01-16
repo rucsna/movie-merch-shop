74483ba1c94b9434491a9d02c4d0c73049522fe3
Anna Pomichal <rucsna@gmail.com>
2024-01-16 14:01:27 +0100

    feat: implement IOrderRepository interface

---
+++ Backend/MovieMerchShop/MovieMerchShop/Service/Repository/OrderRepository.cs
@@ -0,0 +1,40 @@
+using System.Collections.Specialized;
+using System.Data.Entity;
+using MovieMerchShop.Data;
+using MovieMerchShop.Model;
+
+namespace MovieMerchShop.Service.Repository;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public OrderRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Order> CreateNewOrderAsync(Order newOrder)
+    {
+        await _dbContext.Orders.AddAsync(newOrder);
+        await _dbContext.SaveChangesAsync();
+
+        return newOrder;
+    }
+
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await _dbContext.Orders.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Order>> GetByUserIdAsync(Guid userId)
+    {
+        return await _dbContext.Orders.Where(order => order.UserId == userId).ToListAsync();
+    }
+
+    public async Task DeleteOrderAsync(Order orderToDelete)
+    {
+        _dbContext.Orders.Remove(orderToDelete);
+        await _dbContext.SaveChangesAsync();
+    }
+}
