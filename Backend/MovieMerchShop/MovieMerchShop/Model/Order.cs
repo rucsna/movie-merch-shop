4c76ac686a09b321e8fca0e2626cf37c816a1f52
Anna Pomichal <rucsna@gmail.com>
2024-01-17 15:22:36 +0100

    refactor: change userId type to string

--- Backend/MovieMerchShop/MovieMerchShop/Model/Order.cs
+++ Backend/MovieMerchShop/MovieMerchShop/Model/Order.cs
@@ -3,7 +3,7 @@
 public class Order
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
+    public string UserId { get; set; }
     public List<MerchItem> Items { get; set; } = new();
     public DateTime OrderTime { get; set; }
     public decimal OrderSum { get; set; }
