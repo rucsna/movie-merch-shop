c2e26cb286bfc42dafff02fa2a8df818c7995d4d
kovacskitti <kovacskitti001@gmail.com>
2024-01-17 20:25:03 +0100

    docs: add balance component for Profile, style

--- Frontend/vite-project/src/Pages/Profile.jsx
+++ Frontend/vite-project/src/Pages/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-
+import Balance from "../Components/Balance";
 const Profile = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const [upToBalance,setUpToBalance] = useState(false);
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -28,6 +29,10 @@ const Profile = () => {
     fetchUserInfo();
   }, []);
 
+    const handleBalanceClick = () => {
+     setUpToBalance(!upToBalance)
+    };
+
   if (!userInfo) {
     return <p>Loading...</p>;
   }
@@ -37,6 +42,12 @@ const Profile = () => {
       <h2>Profile</h2>
       <p>User name: {userInfo.UserName}</p>
       <p>Email: {userInfo.Email}</p>
+      <p>Balance: {userInfo.Balance}</p>
+      <button onClick={handleBalanceClick}>Top up balance!</button>
+      {upToBalance && <div>
+        <Balance balance={userInfo.Balance} />
+      </div>}
+      <p></p>
     </div>
   );
 };
