1bf51b004c8b3906c09c9c377b21af64ca221119
kovacskitti <kovacskitti001@gmail.com>
2024-01-18 18:55:36 +0100

    feat: change to localstorage form params

--- Frontend/vite-project/src/Pages/Profile.jsx
+++ Frontend/vite-project/src/Pages/Profile.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Balance from "../Components/Balance"
+
 const Profile = () => {
   const [userInfo, setUserInfo] = useState(null);
-  const {email} = useParams()
+  const [upToBalance, setUpToBalance] = useState(false);
+  
+  
   useEffect(() => {
     const fetchUserInfo = async () => {
+      const email = localStorage.getItem("userEmail");
       try {
         const response = await fetch(`/api/Auth/GetUserByEmail/${email}`, {
           method: "GET",
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`, 
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
           },
         });
 
         if (!response.ok) {
-          console.error("Failed to fetch user information:", response.statusText);
+          console.error(
+            "Failed to fetch user information:",
+            response.statusText
+          );
           return;
         }
 
@@ -23,20 +31,39 @@ const Profile = () => {
       } catch (error) {
         console.error("Error during user information retrieval:", error);
       }
+        await fetchUserInfo();
     };
 
     fetchUserInfo();
-  }, []);
+  }, [setUpToBalance]);
 
   if (!userInfo) {
     return <p>Loading...</p>;
   }
 
+    const handleBalanceClick = () => {
+     setUpToBalance(!upToBalance)
+    };
+
+  if (!userInfo) {
+    return <p>Loading...</p>;
+
+     
+  }
+
   return (
     <div className="profile-container">
       <h2>Profile</h2>
-      <p>User name: {userInfo.UserName}</p>
-      <p>Email: {userInfo.Email}</p>
+      <p>User name: {userInfo.userName}</p>
+      <p>Email: {userInfo.email}</p>
+      <p>Balance: {userInfo.balance}</p>
+      <button onClick={handleBalanceClick}>Top up balance!</button>
+      {upToBalance && (
+        <div>
+          <Balance email={userInfo.email} />
+        </div>
+      )}
+      <p></p>
     </div>
   );
 };
