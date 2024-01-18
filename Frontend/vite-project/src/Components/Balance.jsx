25753e0f76ecc0357216e09fd960ddc9ef23851c
kovacskitti <kovacskitti001@gmail.com>
2024-01-18 16:19:20 +0100

    feat: add balance

--- Frontend/vite-project/src/Components/Balance.jsx
+++ Frontend/vite-project/src/Components/Balance.jsx
@@ -1,14 +1,37 @@
-import React, { useEffect, useState } from "react";
-import "../index.css";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const Balance = ({ title, year, imdbID, onClick }) => {
-  const [amount, setAmount] = useState("");
 
+const Balance = ({ email }) => {
+  const [amount, setAmount] = useState("");
   const navigate = useNavigate();
 
   const handleBalance = async (e) => {
     e.preventDefault();
-    console.log("ide jön majd egy fetch");
+
+    try {
+      const balanceResponse = await fetch("/api/Auth/UpBalance", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          balance: parseFloat(amount),
+        }),
+      });
+
+      if (!balanceResponse.ok) {
+        console.error(
+          `Failed to update balance. Status: ${balanceResponse.status}`
+        );
+        return;
+      }
+
+      console.log("Balance updated successfully");
+       navigate("/profile");
+    } catch (error) {
+      console.error("Error during balance update:", error);
+    }
   };
 
   return (
@@ -18,7 +41,7 @@ const Balance = ({ title, year, imdbID, onClick }) => {
         <label>
           Amount:
           <input
-            type="string"
+            type="number" 
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
             required
